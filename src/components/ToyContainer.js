1326204ec3e8cf7d174737b55c8bf9f07f77284f
Brian <brian.sila1@student.moringaschool.com>
2025-05-03 13:02:19 +0300

    display toy information upon render

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [Toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data))
+      .catch((error) => console.log(`Error fetching: `, error));
+  });
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {Toys.map((toy) => {
+        return (
+          <ToyCard
+            key={toy.id}
+            name={toy.name}
+            image={toy.image}
+            likes={toy.likes}
+          />
+        );
+      })}
+    </div>
   );
 }
 
